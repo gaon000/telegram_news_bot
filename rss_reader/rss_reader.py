fb1c06461eef37ce7b63c57bb721f2c1e888d94f
Ubuntu <ubuntu@ip-172-31-37-155.ap-northeast-2.compute.internal>
2020-07-29 11:22:50 +0000

    Fix add try - except

--- rss_reader/rss_reader.py
+++ rss_reader/rss_reader.py
@@ -9,41 +9,47 @@ bot = telegram.Bot(token=my_token)
 
 
 cli = MongoClient('localhost', 27017)
-db = cli.ncs_db
+db = cli.ncs
 collection_breaknews = db.breaknews
-collection_topics = db.topics
+collection_topic = db.topic
 collection_topicnews = db.topicnews
 
 a=[]
 b=[]
 # (t-datetime.datetime(1970,1,1)).total_seconds()
 RSS = {
-    '머니투데이': 'http://rss.mt.co.kr/mt_news_stock.xml',
-    '매일경제':'http://file.mk.co.kr/news/rss/rss_50200011.xml',
-    '한국경제':'http://rss.hankyung.com/new/news_stock.xml',
-    '이데일리':'http://rss.edaily.co.kr/stock_news.xml',
+#'머니투데이': 'http://rss.mt.co.kr/mt_news_stock.xml',
+#'매일경제':'http://file.mk.co.kr/news/rss/rss_50200011.xml',
+#'한국경제':'http://rss.hankyung.com/new/news_stock.xml',
+#'이데일리':'http://rss.edaily.co.kr/stock_news.xml',
     'mk': 'https://www.mk.co.kr/rss/40300001/'
     }
 # '''
 
 while(1):
-    for k, v in RSS.items():
-        print(k)
-        d = feedparser.parse(v)
-        for i in range(len(d['entries'])):
-        # print(d['entries'][0]['title'])
-        # print(d['entries'][0]['link'])
-            a.append(((parser.parse(d['entries'][i]['published']).replace(tzinfo=None) - datetime.datetime(1970,1,1)).total_seconds(), d['entries'][i]['title'], d['entries'][i]['link'], d['entries'][i]['description']))
-    a.sort(key=lambda element : element[0])
-    # for i in collection_topics.find_one()['topics']:
-    #     for j in a:
-    #         if i in j[3]:
-    #             b.append(j)
+	for k, v in RSS.items():
+		print(k)
+		d = feedparser.parse(v)
+		for i in range(len(d['entries'])):
+			try:
+				a.append(((parser.parse(d['entries'][i]['published']).replace(tzinfo=None) - datetime.datetime(1970,1,1)).total_seconds(), d['entries'][i]['title'], d['entries'][i]['link'], d['entries'][i]['description']))
+			except KeyError:
+				print("keyerror")
+	a.sort(key=lambda element : element[0])
+	#	for i in collection_topic.find_one()['topics']:
+#		for j in a:
+#			if i in j[3]:
+#	b.append(j)
                 
-    for i in a:
-        if i[0] > collection_breaknews.find_one()['media']['mk'][0]:
-            print(i)
-            if i == a[-1]:
-                collection_breaknews.replace_one(collection_breaknews.find_one(), {"media":{"mk":a[-1]}})
-    time.sleep(180)
+	for i in a:
+		if i[0] > collection_breaknews.find_one()['media']['mk'][0]:
+			for j in collection_topic.find_one()['topic']:
+				print(j)
+				print(i[3])
+				if j in i[3]:
+					bot.send_message(chat_id=944628369, text=f'topic is {j}\n{i[2]}')
+#bot.send_message(chat_id=944628369, text=i)
+			if i == a[-1]:
+				collection_breaknews.replace_one(collection_breaknews.find_one(), {"media":{"mk":a[-1]}})
+	time.sleep(180)
 # collection_breaknews.insert_one({"media":{"mk":a[-1]}})
