f8e8a325ec489d22451b1e8fe175257c38663543
Ubuntu <ubuntu@ip-172-31-37-155.ap-northeast-2.compute.internal>
2020-08-02 13:24:11 +0000

    fixed untracked files

--- telegram_bot_command.py
+++ telegram_bot_command.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.ncs
 collection = db.topic
+collection_dart = db.dart
 
 #import dart.dart_api as dart
 
@@ -32,7 +33,7 @@ def get_message(bot, update) :
 
 
 def help_command(bot, update) :
-    update.message.reply_text("명령어 목록?\n1. /subscribe: 구독\n2. /topic 키워드: 구독할 topic\n3. /delete 키워드: 삭제할 topic\n4. dart 공시받기")
+    update.message.reply_text("명령어 목록?\n1. /subscribe: 구독\n2. /topic 키워드: 구독할 topic\n3. /delete 키워드: 삭제할 topic\n4. /dart_topic 키워드: dart 공시 받을 토픽입력\n5. /dart_delete 키워드: dart 토픽 삭제")
 
 def subscribe_command(bot, update):
     update.message.reply_text("구독되었습니다. /topic명령어를 이용하여 topic을 추가하여주세요.")
@@ -60,6 +61,28 @@ def topic_sub_command(bot, update, args):
 		bot.send_message(chat_id=944628369, text="%s은(는) 삭제되었습니다."%i)
 	collection.replace_one(collection.find_one(), {'topic':topic_list})
 
+def disclosure_topic_add(bot, update, args):
+	print(args)
+	topics = "구독한 topic:"
+	topic_list = collection_dart.find_one()['topic']
+	topic_list = list(set(topic_list+args))
+	collection_dart.replace_one(collection_dart.find_one(), {'topic':topic_list})
+	for arg in topic_list:
+		topics+='\n - {}'.format(arg)
+	bot.send_message(chat_id=944628369, text = topics)
+
+def disclosure_topic_sub(bot, update, args):
+	topic_list = collection_dart.find_one()['topic']
+	for i in args:
+		try:
+			topic_list.remove(i)
+		except ValueError:
+			bot.send_message(chat_id=944628369, text="%s은(는) 구독하지 않은 토픽입니다."%i)
+			continue
+		bot.send_message(chat_id=944628369, text = "%s은(는) 삭제되었습니다."%i)
+	collection_dart.replace_one(collection_dart.find_one(), {'topic':topic_list})
+	
+
 #def print_news(bot, update, args):
  #   for i in topic:
   #      if i in RSS:
@@ -75,6 +98,7 @@ updater.dispatcher.add_handler(CommandHandler('help', help_command))
 updater.dispatcher.add_handler(CommandHandler('subscribe', subscribe_command))
 updater.dispatcher.add_handler(CommandHandler('topic', topic_add_command, pass_args=True)) 
 updater.dispatcher.add_handler(CommandHandler('delete', topic_sub_command, pass_args=True))
-#updater.dispatcher.add_handler(CommandHandler('dart', dart, pass_args=True))
+updater.dispatcher.add_handler(CommandHandler('dart_topic', disclosure_topic_add, pass_args=True))
+updater.dispatcher.add_handler(CommandHandler('dart_delete', disclosure_topic_sub, pass_args=True)) 
 updater.start_polling(timeout=3, clean=True)
 updater.idle()
